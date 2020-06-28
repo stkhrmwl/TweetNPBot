4741af9759d704489ede0c2a7a71df71d56be32f
TAKAHARA <>
2020-06-28 22:03:37 +0900

    update replyToken on firestore

--- userTrigger/functions/index.js
+++ userTrigger/functions/index.js
@@ -3,6 +3,15 @@
 const line = require('@line/bot-sdk');
 const ENV = require('./env.json');
 
+// ******************** Firestore
+const admin = require('firebase-admin');
+const SERVICE_ACCOUNT = require('./service_account.json');
+admin.initializeApp({
+  credential: admin.credential.cert(SERVICE_ACCOUNT),
+});
+const db = admin.firestore();
+// ********************
+
 const config = {
   channelAccessToken: ENV.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: ENV.LINE_CHANNEL_SECRET,
@@ -12,10 +21,29 @@ const client = new line.Client(config);
 function handleEvent(event) {
   console.log(event);
   let message = 'Please push text message.';
+  let userId;
 
   if (event.type === 'message' && event.message.type === 'text') {
     message = event.message.text;
   }
+
+  // ******************** Firestore
+  userId = event.source.userId;
+
+  if (message.match(/テスト/)) {
+    userId = event.source.userId;
+    try {
+      db.collection('users').doc(userId).set({
+        replyToken: event.replyToken,
+        message: message,
+      });
+    } catch (error) {
+      console.log('更新エラー');
+      console.log(error);
+    }
+    message = 'update db';
+  }
+  // ********************
 
   const echo = { type: 'text', text: message };
   return client.replyMessage(event.replyToken, echo);
