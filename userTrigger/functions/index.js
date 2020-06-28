abe5edaede2312af8585e72d366203154a9464a9
TAKAHARA <>
2020-06-28 21:05:50 +0900

    deploy echo bot

--- userTrigger/functions/index.js
+++ userTrigger/functions/index.js
@@ -1,8 +1,28 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const line = require('@line/bot-sdk');
+const ENV = require('./env.json');
+
+const config = {
+  channelAccessToken: ENV.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: ENV.LINE_CHANNEL_SECRET,
+};
+const client = new line.Client(config);
+
+function handleEvent(event) {
+  console.log(event);
+  let message = 'Please push text message.';
+
+  if (event.type === 'message' && event.message.type === 'text') {
+    message = event.message.text;
+  }
+
+  const echo = { type: 'text', text: message };
+  return client.replyMessage(event.replyToken, echo);
+}
+
+exports.handler = function echoBot(req, res) {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.status(200).send(`Success: ${result}`))
+    .catch((err) => res.status(400).send(err.toString()));
+};
